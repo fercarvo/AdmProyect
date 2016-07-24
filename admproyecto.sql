1d688378363471f2e24180cd841987858e3a9f2a
joyce <jebenite@espol.edu.ec>
2016-07-24 14:16:32 -0500

    nueva base con tabla proyecto y tarea

--- admproyecto.sql
+++ admproyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-07-2016 a las 16:05:38
--- Versión del servidor: 5.7.11
--- Versión de PHP: 5.6.19
+-- Tiempo de generación: 24-07-2016 a las 19:15:14
+-- Versión del servidor: 5.7.9
+-- Versión de PHP: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,35 +23,87 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `proyecto`
+--
+
+DROP TABLE IF EXISTS `proyecto`;
+CREATE TABLE IF NOT EXISTS `proyecto` (
+  `id_proyecto` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` text NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id_proyecto`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_usuario_2` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `proyecto`
+--
+
+INSERT INTO `proyecto` (`id_proyecto`, `descripcion`, `id_usuario`) VALUES
+(1, 'prueba', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tarea`
+--
+
+DROP TABLE IF EXISTS `tarea`;
+CREATE TABLE IF NOT EXISTS `tarea` (
+  `id_tarea` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` text NOT NULL,
+  `estado` text NOT NULL,
+  `id_proyecto` int(11) NOT NULL,
+  PRIMARY KEY (`id_tarea`),
+  KEY `id_proyecto` (`id_proyecto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tarea`
+--
+
+INSERT INTO `tarea` (`id_tarea`, `titulo`, `estado`, `id_proyecto`) VALUES
+(1, 'titulo1', 'inicial', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` text NOT NULL,
   `email` text NOT NULL,
-  `rol` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `rol` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Índices para tablas volcadas
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombre`, `email`, `rol`) VALUES
+(1, 'Joyce', 'joyce@example.com', 'estudiante');
+
+--
+-- Restricciones para tablas volcadas
 --
 
 --
--- Indices de la tabla `usuario`
+-- Filtros para la tabla `proyecto`
 --
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `proyecto`
+  ADD CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Filtros para la tabla `tarea`
 --
+ALTER TABLE `tarea`
+  ADD CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`);
 
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
